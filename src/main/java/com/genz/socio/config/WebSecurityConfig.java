504629a101ba7107ea1208870451ab92f45757b7
Neelesh_Kurmi <neeleshp212@gmail.com>
2025-12-22 21:17:24 +0530

    security setup

---
+++ src/main/java/com/genz/socio/config/WebSecurityConfig.java
@@ -0,0 +1,43 @@
+package com.genz.socio.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebSecurityConfig {
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .authorizeHttpRequests
+                        (a -> a.requestMatchers("api/auth/**").permitAll()
+                        .requestMatchers("/**").hasAnyRole("ADMIN","USER"))
+                .formLogin(Customizer.withDefaults()).build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails user1 = User.withUsername("nilesh")
+                .password(passwordEncoder.encode("nilesh"))
+                .roles("USER").build();
+
+        UserDetails user2 = User.withUsername("admin")
+                .password(passwordEncoder.encode("admin"))
+                .roles("ADMIN").build();
+
+        return new InMemoryUserDetailsManager(user1,user2);
+    }
+
+}
