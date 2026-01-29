2c362e7cb4ae46ff21331da1bb3542b198ae2bbd
neelesh kurmi <=>
2026-01-29 23:31:07 +0530

    issue resolution

--- src/main/java/com/genz/socio/dto/entity/User.java
+++ src/main/java/com/genz/socio/dto/entity/User.java
@@ -14,6 +14,7 @@ import java.util.List;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(name = "users")
 public class User extends BaseEntity{
 
     @Column(unique = true, nullable = false)
